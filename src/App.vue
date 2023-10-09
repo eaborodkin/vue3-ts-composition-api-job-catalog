a980e3edf775b772113ea3210578eca6a423deca
Eugene Borodkin <eaborodkin@gmail.com>
2023-10-09 19:10:37 +0000

    Created JobList component.

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,45 @@
 <script setup lang="ts">
+import type {Job} from "@/types"
+import {ref} from "vue"
+import JobList from "@/components/JobList.vue"
 
+const jobs = ref<Job[]>([
+  {
+    id: 1,
+    title: 'farmer',
+    location: 'village farm',
+    salary: 18000,
+  },
+  {
+    id: 2,
+    title: 'guitar player',
+    location: 'music bar',
+    salary: 15000,
+  },
+  {
+    id: 3,
+    title: 'teacher',
+    location: 'school',
+    salary: 20000,
+  },
+  {
+    id: 4,
+    title: 'doctor',
+    location: 'hospital',
+    salary: 25000,
+  },
+  {
+    id: 5,
+    title: 'taxi driver',
+    location: 'city',
+    salary: 10000,
+  },
+])
 </script>
 
 <template>
 <div class="app">
-  Hello world
+  <JobList :jobs="jobs" />
 </div>
 </template>
 
---
+++ src/components/JobList.vue
@@ -0,0 +1,73 @@
+<script setup lang="ts">
+import type {Job} from "@/types"
+import type {PropType} from "vue"
+
+const props = defineProps({
+  jobs: {
+    required: true,
+    type: Array as PropType<Job[]>,
+  },
+})
+</script>
+
+<template>
+  <div class="jobs">
+    <ul class="jobs__list">
+      <li v-for="job in jobs" :key="job.id" class="jobs__item job">
+        <h2 class="job__title">{{ job.title }} in {{ job.location }}</h2>
+        <div class="job__salary">
+          <p>{{ job.salary }} euros per year</p>
+        </div>
+        <div class="job__description">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores at blanditiis cum distinctio dolorem
+            ex facere harum illum iusto laudantium maxime pariatur quasi unde vitae voluptas voluptatem, voluptates!
+            Distinctio, velit!</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.jobs {
+  max-width: 960px;
+  margin: 40px auto;
+
+  &__list {
+    padding: 0;
+  }
+
+  &__item {
+    list-style-type: none;
+    padding: 16px;
+    margin: 16px 0;
+  }
+}
+
+.job {
+  background: white;
+  border-radius: 8px;
+
+  &__title {
+    margin: 0 0 10px;
+    text-transform: uppercase;
+  }
+
+  &__salary {
+    display: flex;
+
+    & img {
+      width: 30px;
+    }
+
+    & p {
+      color: #1784bf;
+      font-weight: bold;
+      margin: 10px 4px;
+    }
+  }
+
+  &__description {
+  }
+}
+</style>
